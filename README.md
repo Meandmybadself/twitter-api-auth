4cac089d8c2086b8d31ac05aaf15a6fd765949e2
Jeffery Bennett <meandmybadself@gmail.com>
2022-10-21 16:03:16 -0500

    Works?

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 # Twitter API Auth
 
 ## Overview
-When creating bots, I always forget how to generate a refresh token for another user via the OAuth 2 flow.
-This allows us to make a bot that is another user.
+When creating bots, I always forget how to generate a refresh token for another user via the OAuth 1.0 flow.
+I tried creating OAuth 2.0 flows, but I think it required persisting a refresh token.  
+If I can just use the 1.0 token perpetually, that's better.
+
+(If I'm wrong on this, I'd love to know more.)
 
 ## Related
-* https://developer.twitter.com/en/docs/authentication/oauth-2-0/user-access-token
 
-## Environment variables
-You can get from https://developer.twitter.com/en/portal/dashboard
+* https://developer.twitter.com/en/docs/authentication/oauth-1-0a
 
-* CLIENT_ID
-* CLIENT_SECRET
+## Usage
+```
+➜ node index.js
+=================================
 
-## TODOs
-If I reuse this, make it easier by:
+Twitter OAuth 1.0 token generator
 
-* Accepting id & secret via command line.
-* Make into an npx script.
+=================================
+
+Twitter API Key: (YOUR TWITTER API KEY)
+Twitter API Secret: (YOUR TWITTER API SECRET)
+```
+
+This will open a web browser window to authenticate the account you wish to post as.
+
+Authenticate & the page will refresh, showing you API credentials in the form:
+
+```
+{
+  "accessToken": "...",
+  "accessSecret": "...",
+  "userId": "twitter_numerical_user_id",
+  "screenName": "twitter_screenname"
+}
+```
--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 const { TwitterApi } = require("twitter-api-v2");
 const open = require('open');
 const express = require('express')
+const readline = require('node:readline');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+console.log("=================================\n")
+console.log("Twitter OAuth 1.0 token generator\n")
+console.log("=================================\n")
+
+const askQuestion = async (question) => new Promise((resolve, reject) => {
+    rl.question(question, (value) => {
+        value = value.trim()
+        if (value) {
+            return resolve(value)
+        }
+        return reject()
+    });
+})
 
 const port = 3000
-const redirectUri = `http://site.local:${port}/callback` // Make sure this is registered in your app settings
-
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-    console.error('Please set CLIENT_ID and CLIENT_SECRET in your environment')
-}
+const redirectUri = `http://127.0.0.1:${port}/callback` // Make sure this is registered in your app settings
 
 const run = async () => {
-    let codeVerifier
-    let codeChallenge
 
-    const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
+    const appKey = await askQuestion('Twitter API Key: ')
+    const appSecret = await askQuestion('Twitter API Secret: ')
     const app = express()
 
-    app.get('/callback', async (req, res) => {
-        const { code } = req.query
-        const { expiresIn, accessToken, scope, refreshToken } = await client.loginWithOAuth2({ code, codeVerifier, redirectUri })
-        console.log({ expiresIn, accessToken, scope, refreshToken })
-        res.send('Success.  Check console.')
-        process.exit()
-    })
+    let oauth_token
+    let oauth_token_secret
 
     app.listen(port, async () => {
-        const { url, codeVerifier: cv, codeChallenge: cc } = client.generateOAuth2AuthLink(redirectUri, { scope: 'tweet.write offline.access' });
-        codeVerifier = cv
-        codeChallenge = cc
-        await open(url)
+        try {
+            const twitter = new TwitterApi({
+                appKey,
+                appSecret,
+            });
+            const rsp = await twitter.generateAuthLink(redirectUri, {
+                authAccessType: "write",
+                linkMode: "authenticate",
+                forceLogin: true
+            })
+
+            oauth_token = rsp.oauth_token
+            oauth_token_secret = rsp.oauth_token_secret
+
+            await open(rsp.url)
+        } catch (e) {
+            console.error("Error while generating auth link", e)
+            process.exit(1)
+        }
+    })
+
+    app.get('/callback', async (req, res) => {
+        const { oauth_token, oauth_verifier } = req.query
+        const twitter = new TwitterApi({
+            appKey,
+            appSecret,
+            accessToken: oauth_token,
+            accessSecret: oauth_token_secret
+        });
+        const { client, ...args } = await twitter.login(oauth_verifier)
+        res.json(args)
+        process.exit(0)
     })
 }
 
